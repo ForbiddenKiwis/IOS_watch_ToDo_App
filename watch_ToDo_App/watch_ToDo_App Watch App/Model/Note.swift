aa3da9c6b555ac40ed691b815bc66d9cdaff5aa2
ForbiddenKiwis <145590537+ForbiddenKiwis@users.noreply.github.com>
2025-02-19 15:21:32 -0500

    Add files via upload

---
+++ watch_ToDo_App/watch_ToDo_App Watch App/Model/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  watch_ToDo_App Watch App
+//
+//  Created by english on 2025-02-19.
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+    var id: UUID
+    var text: String
+}
